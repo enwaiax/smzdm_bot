98f2dce647ee9f21c1e8fdf7e72e5d6f42f29093
longlegmax <longleg+ss@gmail.com>
2023-02-27 12:31:43 +0800

    fix 本地 docker 运行报错问题
    给定时任务添加了运行参数，解决调用scheduler时报错问题

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,16 +1,16 @@
-import os
-from random import randint
-
 from apscheduler.schedulers.background import BlockingScheduler
 
 from main import main
+from main import main, conf_kwargs
 
 if __name__ == "__main__":
     main()
+    main(conf_kwargs())
     SCH_HOUR = os.environ.get("SCH_HOUR", randint(0, 23))
     SCH_MINUTE = os.environ.get("SCH_MINUTE", randint(0, 59))
     scheduler = BlockingScheduler(timezone="Asia/Shanghai")
     scheduler.add_job(main, "cron", hour=SCH_HOUR, minute=SCH_MINUTE)
+    scheduler.add_job(main, "cron", hour=SCH_HOUR, minute=SCH_MINUTE, args=['conf_kwargs()'])
     print("Press Ctrl+{0} to exit".format("Break" if os.name == "nt" else "C"))
     try:
         scheduler.start()
