5de1f4d2ebc00fd1ee7ffa7be4fe6d574a1e81f8
ikjx <46369637+ikjx@users.noreply.github.com>
2023-09-23 23:10:34 +0800

    Update notify.py
    更换企业微信

--- app/notify/notify.py
+++ app/notify/notify.py
@@ -14,9 +14,66 @@ class NotifyBot(object):
 
         self.push_plus()
         self.server_chain()
-        self.wecom()
+        #self.wecom()
+        self.qiyePush()
+        
         self.tg_bot()
+        
+    def qiyePush(self):
+        """
+        https://www.cnblogs.com/ddzj01/p/12185254.html
+        https://work.weixin.qq.com/api/doc/90001/90143/90372
+        """
+        if not self.kwargs.get("WECOM_BOT_AGENTID", None):
+            logger.warning("⚠️ WECOM_BOT_AGENTID not set, skip WeCom notification")
+            return
+        WECOM_BOT_AGENTID = self.kwargs.get("WECOM_BOT_AGENTID")
+        if not self.kwargs.get("WECOM_BOT_CORPID", None):
+            logger.warning("⚠️ WECOM_BOT_CORPID not set, skip WeCom notification")
+            return
+        WECOM_BOT_CORPID = self.kwargs.get("WECOM_BOT_CORPID")
+        if not self.kwargs.get("WECOM_BOT_SECRET", None):
+            logger.warning("⚠️ WECOM_BOT_SECRET not set, skip WeCom notification")
+            return
+        WECOM_BOT_SECRET = self.kwargs.get("WECOM_BOT_SECRET")
 
+        try:
+            header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.0.0"}
+            # 获取access_token
+            tokenUrl = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
+            Data = {
+                "corpid": WECOM_BOT_CORPID,
+                "corpsecret": WECOM_BOT_SECRET
+            }
+            res_token = requests.get(url=tokenUrl, params=Data,headers=header,verify=False)
+            token = res_token.json()['access_token']
+        
+            # 发送消息
+            msgUrl = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s" % token
+            Data = {
+                "toparty": "1",  # 部门ID
+                "msgtype": "text",  #
+                "agentid": WECOM_BOT_AGENTID,
+                "text": {"content": self.title + '\n' + self.content},
+                "safe": "0"
+            }
+            r = requests.post(url=msgUrl, data=json.dumps(Data), verify=False)
+            n = 1
+            while r.json()['errcode'] != 0 and n < 4:
+                n = n + 1
+                #  重新获取token
+                res_token = requests.get(url=tokenUrl, params=Data, verify=False)
+                token = res_token.json()['access_token']
+                if token:
+                    msgUrl = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s" % token
+                    r = requests.post(url=msgUrl, data=json.dumps(Data), verify=False)
+            if resp.ok:
+                logger.info("✅ WeCom notified")
+            else:
+                logger.warning("Fail to notify WeCom")
+        except Exception as e:
+            logger.error(e)
+    
     def push_plus(self, template="html"):
         if not self.kwargs.get("PUSH_PLUS_TOKEN", None):
             logger.warning("⚠️ PUSH_PLUS_TOKEN not set, skip PushPlus nofitication")
